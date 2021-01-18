456020e5434965f1a1c64c58dbf97e8ce2365dc8
Junichi Tanaka <junichi200123@gmail.com>
2021-01-19 01:40:27 +0900

    change key_hook() interface

--- key_hooks.c
+++ key_hooks.c
@@ -1,11 +1,11 @@
 #include "./cub3d.h"
 
 // キーボード入力のフック
-int 			key_hook(int keycode, t_game *vars)
+int 			key_hook(int keycode, t_game *game)
 {
 	printf("Pressed Key Code: %d\n", keycode);
 	if (keycode == KEY_q || keycode == KEY_esc){
-		mlx_destroy_window(vars->mlx, vars->win);
+		mlx_destroy_window(game->mlx, game->win);
 		exit(0);
 	}
 	return (0);
