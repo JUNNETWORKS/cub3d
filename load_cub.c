fba5cb9087eee2df759d4654fdb2518e56c2c059
Junichi Tanaka <junichi200123@gmail.com>
2021-02-25 19:56:33 +0900

    add: return ERROR if cubfile has 2 R key

--- load_cub.c
+++ load_cub.c
@@ -240,6 +240,8 @@ int	set_resolution(t_game *game, char *width_str, char *height_str)
 	int	max_height;
 
 	printf("width_str: %s, height_str: %s\n", width_str, height_str);
+	if (game->screen_width == 0 || game->screen_height == 0)
+		return (put_and_return_err("Resolution has already configured"));
 	if (!str_all_true(width_str, ft_isdigit) ||
 		!str_all_true(height_str, ft_isdigit) ||
 		(width = ft_atoi(width_str)) <= 0 ||
