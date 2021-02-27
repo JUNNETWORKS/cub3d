27678936bc5d7b95cdc826cc89de1872dd5209ee
Junichi Tanaka <junichi200123@gmail.com>
2021-02-27 13:19:35 +0900

    clean: initialize_player_direction()

--- player.c
+++ player.c
@@ -63,40 +63,29 @@ void		update_player(t_game *game)
 	slide_player(game);
 }
 
+void		initialize_player_direction(t_player *player, char direction)
+{
+	if (direction == 'N' || direction == 'S')
+	{
+		player->dir.x = 0;
+		player->dir.y = direction == 'N' ? -1 : 1;
+		player->plane.x = (direction == 'N' ? 1 : -1) * tan(deg2rad(66 / 2));
+		player->plane.y = 0;
+	}
+	if (direction == 'E' || direction == 'W')
+	{
+		player->dir.x = direction == 'E' ? 1 : -1;
+		player->dir.y = 0;
+		player->plane.x = 0;
+		player->plane.y = (direction == 'E' ? 1 : -1) * tan(deg2rad(66 / 2));
+	}
+}
+
 void		initialize_player(t_player *player, double x, double y, char direction)
 {
-	// プレイヤーの初期座標
 	player->pos.x = x;
 	player->pos.y = y;
-	if (direction == 'N')
-	{
-		player->dir.x = 0;
-		player->dir.y = -1;
-		player->plane.x = 1 * tan(deg2rad(66 / 2));
-		player->plane.y = 0;
-	}
-	if (direction == 'S')
-	{
-		player->dir.x = 0;
-		player->dir.y = 1;
-		player->plane.x = -1 * tan(deg2rad(66 / 2));
-		player->plane.y = 0;
-	}
-	if (direction == 'E')
-	{
-		player->dir.x = 1;
-		player->dir.y = 0;
-		player->plane.x = 0;
-		player->plane.y = 1 * tan(deg2rad(66 / 2));
-	}
-	if (direction == 'W')
-	{
-		player->dir.x = -1;
-		player->dir.y = 0;
-		player->plane.x = 0;
-		player->plane.y = -1 * tan(deg2rad(66 / 2));
-	}
-	// 状態の初期化
+	initialize_player_direction(player, direction);
 	player->is_moving = 0;
 	player->is_sidling = 0;
 	player->is_rotating = 0;
