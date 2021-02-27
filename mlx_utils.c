115c08edc877dd2d91d7327939537a9b4c96a014
Junichi Tanaka <junichi200123@gmail.com>
2021-02-27 12:35:39 +0900

    clean: mlx_utils.c

--- mlx_utils.c
+++ mlx_utils.c
@@ -12,14 +12,16 @@ void		my_mlx_pixel_put(t_img *img, int x, int y, int color)
 
 uint32_t	get_color_from_img(t_img img, int x, int y)
 {
-	return *(uint32_t*)(img.addr + (y * img.line_length + x * (img.bits_per_pixel / 8)));
+	return *(uint32_t*)(img.addr +
+		(y * img.line_length + x * (img.bits_per_pixel / 8)));
 }
 
 int			load_image(t_game *game, t_img *img, char *filepath){
 	img->img = mlx_xpm_file_to_image(game->mlx, filepath, &img->width, &img->height);
 	if (img->img == NULL)
 		return (ERROR);
-    img->addr = mlx_get_data_addr(img->img, &img->bits_per_pixel, &img->line_length, &img->endian);
+    img->addr = mlx_get_data_addr(img->img, &img->bits_per_pixel,
+		&img->line_length, &img->endian);
 	if (img->addr == NULL)
 		return (ERROR);
 	return (0);
