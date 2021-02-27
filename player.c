bc49bf5e4c6ca19c216e8974db629a7d2866d2be
Junichi Tanaka <junichi200123@gmail.com>
2021-02-27 12:52:42 +0900

    fix: rotate_player() and move_player()

--- player.c
+++ player.c
@@ -5,15 +5,18 @@ static void	rotate_player(t_game *game)
 	double	rot_speed;
 	t_vec2	old_vec2;
 
-	rot_speed = game->player.is_rotating * PLAYER_ROTATE_RAD;
+	if (game->player.is_rotating)
+	{
+		rot_speed = game->player.is_rotating * PLAYER_ROTATE_RAD;
 
-	old_vec2 = game->player.dir;
-	game->player.dir.x = old_vec2.x * cos(rot_speed) - old_vec2.y * sin(rot_speed);
-	game->player.dir.y = old_vec2.x * sin(rot_speed) + old_vec2.y * cos(rot_speed);
+		old_vec2 = game->player.dir;
+		game->player.dir.x = old_vec2.x * cos(rot_speed) - old_vec2.y * sin(rot_speed);
+		game->player.dir.y = old_vec2.x * sin(rot_speed) + old_vec2.y * cos(rot_speed);
 
-	old_vec2 = game->player.plane;
-	game->player.plane.x = game->player.plane.x * cos(rot_speed) - game->player.plane.y * sin(rot_speed);
-	game->player.plane.y = old_vec2.x * sin(rot_speed) + game->player.plane.y * cos(rot_speed);
+		old_vec2 = game->player.plane;
+		game->player.plane.x = game->player.plane.x * cos(rot_speed) - game->player.plane.y * sin(rot_speed);
+		game->player.plane.y = old_vec2.x * sin(rot_speed) + game->player.plane.y * cos(rot_speed);
+	}
 }
 
 static void	move_player(t_game *game)
@@ -21,17 +24,20 @@ static void	move_player(t_game *game)
 	double	new_pos_x;
 	double	new_pos_y;
 
-	new_pos_x = game->player.pos.x + game->player.is_moving * game->player.dir.x * PLAYER_MOVE_PX;
-	new_pos_x = new_pos_x < 0 ? 0 : new_pos_x;
-	if (game->map[(int)(game->player.pos.y)][(int)new_pos_x] != '1')
-		game->player.pos.x = new_pos_x;
-		// game->player.pos.x += game->player.is_moving * game->player.dir.x * PLAYER_MOVE_PX;
+	if (game->player.is_moving)
+	{
+		new_pos_x = game->player.pos.x + game->player.is_moving * game->player.dir.x * PLAYER_MOVE_PX;
+		new_pos_x = new_pos_x < 0 ? 0 : new_pos_x;
+		if (game->map[(int)(game->player.pos.y)][(int)new_pos_x] != '1')
+			game->player.pos.x = new_pos_x;
+			// game->player.pos.x += game->player.is_moving * game->player.dir.x * PLAYER_MOVE_PX;
 
-	new_pos_y = game->player.pos.y + game->player.is_moving * game->player.dir.y * PLAYER_MOVE_PX;
-	new_pos_y = new_pos_y < 0 ? 0 : new_pos_y;
-	if (game->map[(int)new_pos_y][(int)(game->player.pos.x)] != '1')
-		game->player.pos.y = new_pos_y;
-		// game->player.pos.y += game->player.is_moving * game->player.dir.y * PLAYER_MOVE_PX;
+		new_pos_y = game->player.pos.y + game->player.is_moving * game->player.dir.y * PLAYER_MOVE_PX;
+		new_pos_y = new_pos_y < 0 ? 0 : new_pos_y;
+		if (game->map[(int)new_pos_y][(int)(game->player.pos.x)] != '1')
+			game->player.pos.y = new_pos_y;
+			// game->player.pos.y += game->player.is_moving * game->player.dir.y * PLAYER_MOVE_PX;
+	}
 }
 
 static void	slide_player(t_game *game)
