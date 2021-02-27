12dce8c57676ee8762fcf82c760b452c31a9898a
Junichi Tanaka <junichi200123@gmail.com>
2021-02-27 12:34:46 +0900

    clean: mlx_utils.c

--- mlx_utils.c
+++ mlx_utils.c
@@ -3,7 +3,7 @@
 // mlx_pixel_put() はレンダリングを待たずに1ピクセルずつ描画するので激遅, なので同じ動きをする関数を自作する
 void		my_mlx_pixel_put(t_img *img, int x, int y, int color)
 {
-    char    *dst;
+    char	*dst;
 
 	// line_lengthは実際のウィンドウの横幅と違うので計算する必要がある
     dst = img->addr + (y * img->line_length + x * (img->bits_per_pixel / 8));
