75ec621f9508ad9e0d7d53b603a0f23313f8e09e
Junichi Tanaka <junichi200123@gmail.com>
2021-02-28 16:52:23 +0900

    norm: bmp.c

--- bmp.c
+++ bmp.c
@@ -1,46 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   bmp.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jtanaka <jtanaka@student.42tokyo.jp>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/28 16:51:53 by jtanaka           #+#    #+#             */
+/*   Updated: 2021/02/28 16:52:07 by jtanaka          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "cub3d.h"
 
-int	write_game2bmp(t_game *game, char *fpath)
+static void	write_file_header(t_game *game, int fd)
 {
-	int			fd;
-	uint32_t	len;
+	uint32_t	tmp;
+
+	write(fd, "BM", 2);
+	tmp = 14 + 10 + game->screen_width * game->screen_height * 4;
+	write(fd, &tmp, 4);
+	write(fd, "\0\0", 2);
+	write(fd, "\0\0", 2);
+	tmp = 14 + 10;
+	write(fd, &tmp, 4);
+}
+
+static void	write_information_header(t_game *game, int fd)
+{
+	uint32_t	tmp;
+
+	tmp = 12;
+	write(fd, &tmp, 4);
+	write(fd, &game->screen_width, 2);
+	write(fd, &game->screen_height, 2);
+	tmp = 1;
+	write(fd, &tmp, 2);
+	tmp = 32;
+	write(fd, &tmp, 2);
+}
+
+static void	write_image_data(t_game *game, int fd)
+{
 	uint32_t	tmp;
 	int			x;
 	int			y;
 
-	if ((fd = open(fpath, O_WRONLY | O_CREAT, S_IRWXU | S_IRWXG | S_IRWXO)) == -1)
-		return (-1);
-	
-	// File Header
-	// bfType
-	write(fd, "BM", 2);
-	// bfSize  ファイルサイズ[byte]
-	len = 14 + 10 + game->screen_width * game->screen_height * 4;  // FileHeader + InformationHeader + imagedata
-	write(fd, &len, 4);
-	// bfReserved1
-	write(fd, "\0\0", 2);
-	// bfReserved2
-	write(fd, "\0\0", 2);
-	// bfOffBits  ファイル先頭から画像データまでのオフセット[byte]
-	len = 14 + 10;
-	write(fd, &len, 4);
-	
-	// Information Header
-	// bcSize
-	tmp = 12;
-	write(fd, &tmp, 4);
-	// bcWidth  画像の幅[ピクセル]
-	write(fd, &game->screen_width, 2);
-	// bcHeight 画像の高さ[ピクセル]
-	write(fd, &game->screen_height, 2);
-	// bcPlanes
-	tmp = 1;
-	write(fd, &tmp, 2);
-	// bcBitCount
-	tmp = 32;
-	write(fd, &tmp, 2);
-	
-	// Image Data
 	y = game->screen_height - 1;
 	while (y >= 0)
 	{
@@ -53,6 +57,18 @@ int	write_game2bmp(t_game *game, char *fpath)
 		}
 		y--;
 	}
+}
+
+int			write_game2bmp(t_game *game, char *fpath)
+{
+	int			fd;
+
+	if ((fd = open(fpath, O_WRONLY | O_CREAT,
+			S_IRWXU | S_IRWXG | S_IRWXO)) == -1)
+		return (-1);
+	write_file_header(game, fd);
+	write_information_header(game, fd);
+	write_image_data(game, fd);
 	close(fd);
 	return (0);
 }
