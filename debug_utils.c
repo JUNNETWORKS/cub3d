9c5762ab1f927d38a47f48834b45a9f5feae0898
Junichi Tanaka <junichi200123@gmail.com>
2021-02-28 16:53:53 +0900

    norm: debug_utils.c

--- debug_utils.c
+++ debug_utils.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   debug_utils.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jtanaka <jtanaka@student.42tokyo.jp>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/28 16:53:02 by jtanaka           #+#    #+#             */
+/*   Updated: 2021/02/28 16:53:37 by jtanaka          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "cub3d.h"
 
 void	print_game(t_game *game)
@@ -6,8 +18,10 @@ void	print_game(t_game *game)
 	printf("player\n");
 	printf("pos\n\tx: %lf\n\ty: %lf\n", game->player.pos.x, game->player.pos.y);
 	printf("dir\n\tx: %lf\n\ty: %lf\n", game->player.dir.x, game->player.dir.y);
-	printf("plane\n\tx: %lf\n\ty: %lf\n", game->player.plane.x, game->player.plane.y);
-	printf("status\n\tis_moving: %d\n\tis_rotating: %d\n", game->player.is_moving, game->player.is_rotating);
+	printf("plane\n\tx: %lf\n\ty: %lf\n",
+		game->player.plane.x, game->player.plane.y);
+	printf("status\n\tis_moving: %d\n\tis_rotating: %d\n",
+		game->player.is_moving, game->player.is_rotating);
 }
 
 void	print_vec2(t_vec2 vec)
