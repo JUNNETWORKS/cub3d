54d99ebe72fd5a61dcdce22d46e102bc821c2d23
Junichi Tanaka <junichi200123@gmail.com>
2021-03-01 00:20:04 +0900

    add: put_and_exit_err()

--- error_utils.c
+++ error_utils.c
@@ -6,7 +6,7 @@
 /*   By: jtanaka <jtanaka@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/28 18:10:23 by jtanaka           #+#    #+#             */
-/*   Updated: 2021/02/28 18:10:24 by jtanaka          ###   ########.fr       */
+/*   Updated: 2021/03/01 00:18:56 by jtanaka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,4 +22,10 @@ int			put_and_return_err(char *str)
 {
 	put_error_msg(str);
 	return (ERROR);
+}
+
+int			put_and_exit_err(char *str)
+{
+	put_error_msg(str);
+	exit(EXIT_FAILURE);
 }
