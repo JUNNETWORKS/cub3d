4f4814b99ef615e1e46f3d4b41048567b81f555a
Junichi Tanaka <junichi200123@gmail.com>
2021-02-28 18:17:45 +0900

    norm: load_color.c

--- load_color.c
+++ load_color.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   load_color.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jtanaka <jtanaka@student.42tokyo.jp>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/28 18:16:03 by jtanaka           #+#    #+#             */
+/*   Updated: 2021/02/28 18:17:20 by jtanaka          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "cub3d.h"
 
 bool		is_valid_color(int r, int g, int b)
@@ -19,10 +31,12 @@ uint32_t	get_color_from_rgbstr(char *rgbstr, uint32_t *color)
 	if (!str_all_true(rgb[0], ft_isdigit) ||
 			!str_all_true(rgb[1], ft_isdigit) ||
 			!str_all_true(rgb[2], ft_isdigit) ||
-			!is_valid_color(ft_atoi(rgb[0]), ft_atoi(rgb[1]), ft_atoi(rgb[2])) ||
+			!is_valid_color(ft_atoi(rgb[0]),
+				ft_atoi(rgb[1]), ft_atoi(rgb[2])) ||
 			(rgb[0][0] == '0' && rgb[0][1]) ||
 			(rgb[1][0] == '0' && rgb[1][1]) ||
-			(rgb[2][0] == '0' && rgb[2][1])){
+			(rgb[2][0] == '0' && rgb[2][1]))
+	{
 		free_ptrarr((void**)rgb);
 		return (ERROR);
 	}
@@ -31,7 +45,7 @@ uint32_t	get_color_from_rgbstr(char *rgbstr, uint32_t *color)
 	return (0);
 }
 
-bool	has_color_set(t_game *game, char name)
+bool		has_color_set(t_game *game, char name)
 {
 	if (name == 'F' && game->ground_color == UINT32_MAX)
 		return (false);
@@ -40,7 +54,7 @@ bool	has_color_set(t_game *game, char name)
 	return (true);
 }
 
-int		set_color(t_game *game, char name, char *rgbstr)
+int			set_color(t_game *game, char name, char *rgbstr)
 {
 	char		**rgb;
 	uint32_t	color;
