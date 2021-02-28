fd71ca35c7c787937e7c51566b433a054a00c0ed
Junichi Tanaka <junichi200123@gmail.com>
2021-03-01 00:47:19 +0900

    remove: print input str in load_resolution()

--- load_resolution.c
+++ load_resolution.c
@@ -6,7 +6,7 @@
 /*   By: jtanaka <jtanaka@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/28 23:57:10 by jtanaka           #+#    #+#             */
-/*   Updated: 2021/02/28 23:57:11 by jtanaka          ###   ########.fr       */
+/*   Updated: 2021/03/01 00:45:08 by jtanaka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,6 @@ int		set_resolution(t_game *game, char *width_str, char *height_str)
 	int	width;
 	int	height;
 
-	printf("width_str: %s, height_str: %s\n", width_str, height_str);
 	if (game->screen_width || game->screen_height)
 		return (put_and_return_err("Resolution has already configured"));
 	if (!str_all_true(width_str, ft_isdigit) ||
