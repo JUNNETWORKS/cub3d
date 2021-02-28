514a57dd180242cf70672a819afbb90dd5dcd513
Junichi Tanaka <junichi200123@gmail.com>
2021-02-28 23:49:16 +0900

    norm: sprite.c

--- sprite.c
+++ sprite.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sprite.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jtanaka <jtanaka@student.42tokyo.jp>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/28 23:48:36 by jtanaka           #+#    #+#             */
+/*   Updated: 2021/02/28 23:48:37 by jtanaka          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "cub3d.h"
 
 int				add_sprite(t_game *game, double x, double y)
@@ -10,7 +22,8 @@ int				add_sprite(t_game *game, double x, double y)
 	if (!(new_sprites = malloc(sizeof(t_vec2) * (game->sprite_num + 1))))
 		return (ERROR);
 	if (game->sprites)
-		ft_memcpy(new_sprites, game->sprites, sizeof(t_vec2) * game->sprite_num);
+		ft_memcpy(new_sprites, game->sprites,
+			sizeof(t_vec2) * game->sprite_num);
 	free(game->sprites);
 	new_sprites[game->sprite_num++] = new;
 	game->sprites = new_sprites;
@@ -22,7 +35,8 @@ static void		calc_sprite_dists(t_game *game)
 	int i;
 
 	i = 0;
-	while (i < game->sprite_num){
+	while (i < game->sprite_num)
+	{
 		game->sprite_dists[i] = (
 		(game->player.pos.x - game->sprites[i].x) *
 		(game->player.pos.x - game->sprites[i].x) +
@@ -32,7 +46,7 @@ static void		calc_sprite_dists(t_game *game)
 	}
 }
 
-static void 	swap_sprite(t_game *game, int a, int b)
+static void		swap_sprite(t_game *game, int a, int b)
 {
 	double	tmp;
 	t_vec2	tmpvec2;
@@ -52,11 +66,14 @@ void			sort_sprites(t_game *game)
 
 	calc_sprite_dists(game);
 	flag = 1;
-	while (flag){
+	while (flag)
+	{
 		flag = 0;
 		i = game->sprite_num - 1;
-		while (i > 0){
-			if (game->sprite_dists[i] > game->sprite_dists[i-1]){
+		while (i > 0)
+		{
+			if (game->sprite_dists[i] > game->sprite_dists[i - 1])
+			{
 				swap_sprite(game, i, i - 1);
 				flag = 1;
 			}
